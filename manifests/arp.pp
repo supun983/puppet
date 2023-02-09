6e8c44dcd76c856636107328e18a6cd6bbd04e23
supun983 <51007851+supun983@users.noreply.github.com>
2023-02-09 17:06:41 +0530

    Update arp.pp

--- manifests/arp.pp
+++ manifests/arp.pp
@@ -1,8 +1,31 @@
+# Define interfaces list
+$interfaces = [
+  'eth0',
+  'eth1',
+  'eth2',
+]
+
+# List of emaisl
+$emails = [
+  'user1@example.com',
+  'user2@example.com',
+  'user3@example.com',
+]
+
 #Arpwatch installation
 package { 'arpwatch':
   ensure   => '2.1a15-7',
   provider => 'apt',
 }
+
+# Create a separate configuration file for each interface
+$interfaces.each |String $iface| {
+  file { "/etc/arpwatch/#{$iface}.iface":
+    content => template("module/arpwatch.#{$iface}.iface.erb"),
+    notify  => Service['arpwatch'],
+  }
+}
+
 #arpwatch service
 service { 'arpwatch':
   ensure   => 'running',
