240471dbc3f4a1eb81a39db8b6e09e66ed6ee4bc
supun983 <51007851+supun983@users.noreply.github.com>
2023-02-09 17:57:59 +0530

    Update arp.pp

--- manifests/arp.pp
+++ manifests/arp.pp
@@ -14,6 +14,9 @@ package { 'arpwatch':
 # Create a separate configuration file for each interface
 file { "/etc/arpwatch/#{$iface}.iface":
   content => "interface: #{$iface}\nmail-to: #{$email_addresses.join(', ')}\n",
+  ensure => present,
+  owner => 'root',
+  mode => '0644',
   notify  => Service['arpwatch'],
 }
 
