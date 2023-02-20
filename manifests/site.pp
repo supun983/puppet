b68e842987d20536d17c4023b34794b828fd041d
supun983 <51007851+supun983@users.noreply.github.com>
2023-02-20 15:18:22 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,1 +1,26 @@
+#Arpwatch installation
+package { 'arpwatch':
+  ensure   => '2.1a15-7',
+  provider => 'apt',
+}
+
+# Configuration file
+$iface.each |String $iface| {
+  file { "/etc/arpwatch/${iface}.iface":
+    content => "IFACE_ARGS=\"-m ${email_addresses.join(',')}\"\n",
+    ensure  => present,
+    owner   => 'root',
+    mode    => '0644',
+    notify  => Service["arpwatch@${iface}.service"],
+  }
+}
+
+#arpwatch service
+$iface.each |String $iface| {
+  service { "arpwatch@${iface}.service":
+    ensure    => 'running',
+    enable    => 'true',
+    subscribe => [ Package['arpwatch'], File["/etc/arpwatch/${iface}.iface"] ],
+  }
+}
 
