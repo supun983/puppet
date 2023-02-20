4f4760b6a413c881f533c2ac5d4a0c2393ee6c59
supun983 <51007851+supun983@users.noreply.github.com>
2023-02-20 15:22:47 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,26 +1,29 @@
-#Arpwatch installation
-package { 'arpwatch':
-  ensure   => '2.1a15-7',
-  provider => 'apt',
-}
+class arpwatch_vlans {
+  $emails = hiera_array('emails')
+  $iface = hiera_array('arpwatch_vlans')
 
-# Configuration file
-$iface.each |String $iface| {
-  file { "/etc/arpwatch/${iface}.iface":
-    content => "IFACE_ARGS=\"-m ${email_addresses.join(',')}\"\n",
-    ensure  => present,
-    owner   => 'root',
-    mode    => '0644',
-    notify  => Service["arpwatch@${iface}.service"],
+  package { 'arpwatch':
+    ensure   => '2.1a15-7',
+    provider => 'apt',
+  }
+
+  # Configuration file
+  $iface.each |String $iface| {
+    file { "/etc/arpwatch/${iface}.iface":
+      content => "IFACE_ARGS=\"-m ${emails.join(',')}\"\n",
+      ensure => present,
+      owner => 'root',
+      mode => '0644',
+      notify => Service["arpwatch@${iface}.service"],
+    }
+  }
+
+  #arpwatch service
+  $iface.each |String $iface| {
+    service { "arpwatch@${iface}.service":
+      ensure => 'running',
+      enable => 'true',
+      subscribe => Package['arpwatch'],
+    }
   }
 }
-
-#arpwatch service
-$iface.each |String $iface| {
-  service { "arpwatch@${iface}.service":
-    ensure    => 'running',
-    enable    => 'true',
-    subscribe => [ Package['arpwatch'], File["/etc/arpwatch/${iface}.iface"] ],
-  }
-}
-
